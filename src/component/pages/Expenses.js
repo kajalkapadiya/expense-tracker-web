4541338790c44b6dea945218ed7e6681b8efb087
kajalkapadiya <kajalkapadiya18@gmail.com>
2023-05-29 19:23:28 +0530

    adding expenses to the firebase realtime database

--- src/component/pages/Expenses.js
+++ src/component/pages/Expenses.js
@@ -1,19 +1,43 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Button } from "react-bootstrap";
 
 const Expenses = () => {
+  const [data, setData] = useState();
   const amountRef = useRef();
   const descriptionRef = useRef();
   const categoryRef = useRef();
 
-  const expenseHandler = (e) => {
+  const expenseHandler = async (e) => {
     e.preventDefault();
     const amount = amountRef.current.value;
     const description = descriptionRef.current.value;
     const category = categoryRef.current.value;
-    localStorage.setItem("amount", amount);
-    localStorage.setItem("description", description);
-    localStorage.setItem("category", category);
+
+    // add to firebase.
+
+    const response = await fetch(
+      "https://expensetracker-677bd-default-rtdb.firebaseio.com/expenses.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          amount,
+          description,
+          category,
+        }),
+        headers: { "content-type": "application/json" },
+      }
+    );
+    console.log(response);
+    if (response) {
+      setData({
+        amount: amount,
+        description: description,
+        category: category,
+      });
+    } else {
+      alert("fill the data");
+    }
+    console.log(data);
   };
 
   return (
@@ -35,11 +59,7 @@ const Expenses = () => {
           submit
         </Button>
       </form>
-      <p>
-        <h4>{localStorage.getItem("description")}</h4>
-        <h4>{localStorage.getItem("category")}</h4>
-        <h4>{localStorage.getItem("amount")}</h4>
-      </p>
+     
     </>
   );
 };
