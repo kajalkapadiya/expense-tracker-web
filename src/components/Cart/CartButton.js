e7a135033b00f1a22702fa8d4aa86a6b1587b57a
kajalkapadiya <121010624+kajalkapadiya@users.noreply.github.com>
2023-06-11 21:32:29 +0530

    part-2
    all the buttons start to working now.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,9 +1,10 @@
 import classes from "./CartButton.module.css";
 import { uiActions } from "../store/uiSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const cartHandler = () => {
     dispatch(uiActions.toggle());
@@ -11,7 +12,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={cartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
