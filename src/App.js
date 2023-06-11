ecd89aff6967fe2eb7c9b90cbf4dc0d6639a1e79
kajalkapadiya <121010624+kajalkapadiya@users.noreply.github.com>
2023-06-11 16:25:47 +0530

    src file
    extra file created in different file for product page.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
-import { useContext } from "react";
-import "./App.css";
-import { Redirect, Route, Switch } from "react-router-dom";
-import Home from "./component/pages/Home";
-import Product from "./component/pages/Product";
-import MainHeader from "./component/auth/MainHeader";
-import Login from "./component/pages/Login";
-import About from "./component/pages/About";
-import AuthContext from "./component/auth/AuthContext";
-import Profile from "./component/pages/Profile";
-import ForgotPass from "./component/ForgotPass";
+import { useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
-  const authCntx = useContext(AuthContext);
-
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
   return (
-    <>
-      <MainHeader />
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home">
-          {authCntx.isLoggedIn && <Home />}
-          {!authCntx.isLoggedIn && <Redirect to="/login" />}
-        </Route>
-        <Route path="/product">
-          {authCntx.isLoggedIn && <Product />}
-          {!authCntx.isLoggedIn && <Redirect to="/login" />}
-        </Route>
-        <Route path="/about">
-          {authCntx.isLoggedIn && <About />}
-          {!authCntx.isLoggedIn && <Redirect to="/login" />}
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/forgot">
-          <ForgotPass />
-        </Route>
-      </Switch>
-    </>
+    <Layout>
+      {showCart && <Cart />}
+      <Products />
+    </Layout>
   );
 }
 
---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,19 @@
+import classes from "./CartButton.module.css";
+import { uiActions } from "../store/uiSlice";
+import { useDispatch } from "react-redux";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const cartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+  return (
+    <button className={classes.button} onClick={cartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
